6e220bd26d4f1cff3d28f6a5dd14d84a84f2fa81
Pranaya Shrestha <pshrest2@go.olemiss.edu>
2021-01-27 12:06:13 -0600

    user signup, singin, singout APIs built successfully

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,8 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken"); //to generate signed token
+const expressJwt = require("express-jwt"); //for authorization check
+
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
   console.log("req.body", req.body);
@@ -7,11 +11,49 @@ exports.signup = (req, res) => {
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
-        err,
+        err: errorHandler(err),
       });
     }
+    user.salt = undefined;
+    user.hashed_password = undefined;
     res.json({
       user,
     });
   });
+};
+
+exports.signin = (req, res) => {
+  //find user based on email
+
+  const { email, password } = req.body;
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        err: "User with that email does not exist. Please signup",
+      });
+    }
+    //if user is found, make sure the email and password match
+    //create authenticate method in user model
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        err: "Email or Password does not match",
+      });
+    }
+
+    //generate a signed token with user id and secret
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+    //persist the token as 't' in cookie with expiry date
+    res.cookie("t", token, { expire: new Date() + 9999 });
+
+    //return response with user and token to frontend client
+    const { _id, name, email, role } = user;
+
+    return res.json({ token, user: { _id, email, name, role } });
+  });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie("t");
+  res.json({ message: "Signout Success" });
 };
