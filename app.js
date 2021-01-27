fa0e8fb0ebaf400ab6de615e1aed3667f658d7fb
Pranaya Shrestha <pshrest2@go.olemiss.edu>
2021-01-27 10:57:32 -0600

    user signup post request built

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
+// import routes
+const userRoutes = require("./routes/user");
 
-//db
+// app
+const app = express();
+
+// db
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("DB Connected");
   })
@@ -18,12 +29,15 @@ mongoose.connection.on("error", (err) => {
   console.log(`DB connection error: ${err.message}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello from node");
-});
+//middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// routes middleware
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
