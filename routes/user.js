8018eaf8addfd660d0a857d2c692ffe95113afaa
Pranaya Shrestha <pshrest2@go.olemiss.edu>
2021-01-28 12:53:31 -0600

    secrets/:userId route added such that a person needs to log in to see his/her profile. Also, once logged in, they cannot check out other's profiles. This is done using isAuth method. Likewise, the user need to me admin to access the secrets/:userId page. That is done using the isAdmin method. These new methods are  in the user.js contoller

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { userSignupValidator } = require("../validator");
-const { signup, signin, signout } = require("../controllers/user");
+const { userById } = require("../controllers/user");
 
-router.post("/signup", userSignupValidator, signup);
-router.post("/signin", signin);
-router.get("/signout", signout);
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
+
+router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
+  res.json({
+    user: req.profile,
+  });
+});
+router.param("userId", userById);
 
 module.exports = router;
