ce118709e97f261f0e99e751fd97b3f9aa1158f2
Pranaya Shrestha <pshrest2@go.olemiss.edu>
2021-01-30 19:43:53 -0600

    view/update user profile feature added

--- controllers/user.js
+++ controllers/user.js
@@ -11,3 +11,28 @@ exports.userById = (req, res, next, id) => {
     next();
   });
 };
+
+exports.read = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+
+  return res.json(req.profile);
+};
+
+exports.update = (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Not Authorized to update this profile",
+        });
+      }
+      user.salt = undefined;
+      user.hashed_password = undefined;
+      res.json(user);
+    }
+  );
+};
