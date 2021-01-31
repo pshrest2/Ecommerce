1f4ed8d50b4068d278d7fddaec1bcddc2309246c
Pranaya Shrestha <pshrest2@go.olemiss.edu>
2021-01-30 19:25:26 -0600

    get product photo by id

--- controllers/product.js
+++ controllers/product.js
@@ -192,3 +192,66 @@ exports.listRelated = (req, res) => {
       res.json(products);
     });
 };
+
+exports.listCategories = (req, res) => {
+  Product.distinct("category", {}, (err, categories) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Categories not found",
+      });
+    }
+    res.json(categories);
+  });
+};
+
+exports.listBySearch = (req, res) => {
+  let order = req.query.order ? req.query.order : "desc";
+  let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+  let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+  let skip = parseInt(req.body.skip);
+  let findArgs = {};
+
+  for (let key in req.body.filters) {
+    console.log(key);
+    console.log("----------------------------------");
+
+    if (req.body.filters[key].length > 0) {
+      if (key === "price") {
+        // gte -  greater than price [0-10]
+        // lte - less than
+        findArgs[key] = {
+          $gte: req.body.filters[key][0],
+          $lte: req.body.filters[key][1],
+        };
+      } else {
+        findArgs[key] = req.body.filters[key];
+      }
+    }
+  }
+
+  Product.find(findArgs)
+    .select("-photo")
+    .populate("category")
+    .sort([[sortBy, order]])
+    .skip(skip)
+    .limit(limit)
+    .exec((err, data) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Products not found",
+        });
+      }
+      res.json({
+        size: data.length,
+        data,
+      });
+    });
+};
+
+exports.photo = (req, res, next) => {
+  if (req.product.photo.data) {
+    res.set("Content-Type", req.product.photo.contentType);
+    return res.send(req.product.photo.data);
+  }
+  next();
+};
--- routes/product.js
+++ routes/product.js
@@ -9,12 +9,16 @@ const {
   update,
   list,
   listRelated,
+  listCategories,
+  listBySearch,
+  photo,
 } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
-router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.post("/products/by/search", listBySearch);
+
 router.delete(
   "/product/:productId/:userId",
   requireSignin,
@@ -22,6 +26,7 @@ router.delete(
   isAdmin,
   remove
 );
+
 router.put(
   "/product/:productId/:userId",
   requireSignin,
@@ -31,7 +36,10 @@ router.put(
 );
 
 router.get("/products", list);
+router.get("/product/:productId", read);
 router.get("/products/related/:productId", listRelated);
+router.get("/products/categories", listCategories);
+router.get("/product/photo/:productId", photo);
 
 router.param("productId", productById);
 router.param("userId", userById);
