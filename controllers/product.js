3008e54cd8ad43605b9ec505fd108d6203780f99
Pranaya Shrestha <pshrest2@go.olemiss.edu>
2021-03-15 18:02:49 -0500

    braintree setup backend

--- controllers/product.js
+++ controllers/product.js
@@ -6,15 +6,17 @@ const fs = require("fs");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.productById = (req, res, next, id) => {
-  Product.findById(id).exec((err, product) => {
-    if (err || !product) {
-      res.status(400).json({
-        error: "Product not found",
-      });
-    }
-    req.product = product;
-    next();
-  });
+  Product.findById(id)
+    .populate("category")
+    .exec((err, product) => {
+      if (err || !product) {
+        res.status(400).json({
+          error: "Product not found",
+        });
+      }
+      req.product = product;
+      next();
+    });
 };
 
 exports.read = (req, res) => {
