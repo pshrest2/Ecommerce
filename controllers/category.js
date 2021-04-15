5092e5427df3d335048c0833c584a9c84562f970
Pranaya Shrestha <pshrest2@go.olemiss.edu>
2021-04-14 23:36:01 -0500

    category update working backend

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,7 @@
 const Category = require("../models/category");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+//read a single category by id
 exports.categoryById = (req, res, next, id) => {
   Category.findById(id).exec((err, category) => {
     if (err || !category) {
@@ -14,6 +15,7 @@ exports.categoryById = (req, res, next, id) => {
   });
 };
 
+//create a new category
 exports.create = (req, res) => {
   const category = new Category(req.body);
   category.save((err, data) => {
@@ -27,10 +29,12 @@ exports.create = (req, res) => {
   });
 };
 
+//read a category
 exports.read = (req, res) => {
   return res.json(req.category);
 };
 
+//delete a category
 exports.remove = (req, res) => {
   const category = req.category;
 
@@ -46,6 +50,7 @@ exports.remove = (req, res) => {
   });
 };
 
+//update a category
 exports.update = (req, res) => {
   const category = req.category;
   category.name = req.body.name;
@@ -60,6 +65,7 @@ exports.update = (req, res) => {
   });
 };
 
+//read all categories
 exports.list = (req, res) => {
   Category.find().exec((err, data) => {
     if (err) {
--- controllers/user.js
+++ controllers/user.js
@@ -3,8 +3,6 @@ const { Order } = require("../models/order");
 const User = require("../models/user");
 
 exports.userById = (req, res, next, id) => {
-  console.log(id);
-  console.log("test");
   User.findById(id).exec((err, user) => {
     if (err || !user) {
       return res.status(400).json({
